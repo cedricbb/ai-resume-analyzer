72b4630e66cada04abfe710d11c7080854bd8925
Cédric BILLARD <cbillard@com-company.fr>
2025-10-15 11:54:42 +0200

    auth config + adding puter

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,18 +14,17 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-          <div className="page-heading">
+          <div className="page-heading py-16">
               <h1>Track your Applications & Resume Ratings</h1>
               <h2>Review your submissions and check AI-powered feedback.</h2>
           </div>
+          {resumes.length > 0 && (
+            <section className="resumes-section">
+              {resumes.map((resume: Resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))}
+            </section>
+          )}
       </section>
-
-      {resumes.length > 0 && (
-        <section className="resumes-section">
-          {resumes.map((resume: Resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
-          ))}
-        </section>
-      )}
   </main>;
 }
